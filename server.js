86a5a4ff6a555a5be4f2fe87df22b386d23be085
aditi9012 <varshneyaditi1999@gmail.com>
2020-09-19 16:47:48 +0530

    initial commit

--- server.js
+++ server.js
@@ -11,6 +11,32 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get('/test',(req,res)=>{
 res.json({message:"welcome!!!"});
 });
+const db = require("./app/models");
+
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
